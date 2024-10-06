6b0911e576989dfa662a49674618873594f113c7
Aybulat Yuldashbaev <yuldashbaev@gmail.com>
2024-10-06 14:29:11 +0500

    commit

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
     TELEGRAM_CHANNEL: process.env.TELEGRAM_CHANNEL,
+    DOMAIN: process.env.DOMAIN,
     DB_CONFIG: {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
@@ -10,11 +11,6 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
     },
-    DOMAIN: 'bot.fatir.su',
-    cityChannels: {
-        'Алматы': '@rent_apartment_almaty',
-        'Астана': '@rent_apartment_astana',
-        'Шымкент': '@rent_apartment_shymkent',
-    },
-    CHECK_INTERVAL:'24 hours', // '10 seconds', '5 minutes', '24 hours', '7 days', '1 month', '1 year', '1 day 3 hours 15 minutes'
+    cityChannels: JSON.parse(process.env.CITY_CHANNELS || '{}'),
+    CHECK_INTERVAL:'24 hours', // '10 seconds', '5 minutes', '24 hours', '7 days', '1 month', '1 year','1 day 3 hours 15 minutes'
 };
